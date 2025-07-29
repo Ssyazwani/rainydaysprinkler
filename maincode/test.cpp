fd819bea05533174747a433cdbf3e3bd40da4e21
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-07-30 00:45:40 +0800

    fixing the LED inputs

--- maincode/test.cpp
+++ maincode/test.cpp
@@ -1,15 +1,23 @@
 //first testcode with prototype
 
+//H3 - Ultrasonic Level - PA0, PA2
+//H5 - LED - PC0,PC1,PC2
+//H6 - Valve - PB1
+//H7 - Soil Moisture - PC8,PC9
+
 #include "mbed.h"
 
-DigitalOut tigger(D1);
-DigitalIn echo (D2);
+DigitalOut trigger(PA_0);
+DigitalIn echo (PA_2);
 
-AnalogIn soilSensor(A0);
-DigitalOut dryLed(LED1);
+AnalogIn soilSensor(PC_8);
 
-DigitalIn controlInput(D3);
-DigitalOut valve(D4);
+DigitalOut red_LED(PC_0);
+DigitalOut green_LED(PC_1);
+DigitalOut blue_LED(PC_2);
+
+DigitalIn controlInput(PB1);
+DigitalOut valve(PB_1);
 
 Timer echoTimer;
 
@@ -21,7 +29,7 @@ void delay_us(int us){
 }
 
 float getDistanceCM(){
-    tigger =1;
+    trigger =1;
     wait_us(10);
     trigger =0;
 
@@ -30,10 +38,10 @@ float getDistanceCM(){
         echoTimer.start();
 
     while(echo ==1);
-    echoTimer.stop;
+    echoTimer.stop();
 
-    float duration_us = echoTimer.read.us();
-    float distance =(uratio_us *SPEED_OF_SOUND_CM_PER_US)/2.0f;
+    float duration_us = echoTimer.read_us();
+    float distance =(duration_us *SPEED_OF_SOUND_CM_PER_US)/2.0f;
     return distance;
 }
 
@@ -56,9 +64,14 @@ int main(){
         printf("Soil Moisture: %.3f\n",moisture);
 
         if (moisture < WET_THRSEHOLD){
-            dryLED = 1; // LED ON if soil is dry
+            red_LED = 1;
+            green_LED = 0;
+            blue_LED = 0;
+
         } else {
-            dryLED = 0;
+            red_LED = 0;
+            green_LED = 0;
+            blue_LED = 0;
         }
 
         //Valve
